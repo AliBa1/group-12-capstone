1fc72e7d8dbe662ae2784483436bd3383fac8126
Ali <89438079+AliBa1@users.noreply.github.com>
2024-10-13 22:14:59 -0700

    setup tailwind and daisyui

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Group 12 Capstone Project
 
-## Before starting development
+### Before starting development
 
 - Make sure python3 is on your machine <br />
   Version: Python 3.13.0
@@ -21,14 +21,15 @@ DB_PORT = 5432
 
 - Setup Ruff with your code editor: https://docs.astral.sh/ruff/editors/setup/#vs-code
 
-## Run Project
+### Run Project
 
 - Enter python environment.<br /> Linux/Mac: `source env/bin/activate` <br /> Windows: `.\env\Scripts\activate`
 - Install dependencies: `pip install -r requirements.txt`
 - Enter project directory: `cd project`
 - Run server `python manage.py runserver` and follow instructions on terminal
+- (optional) To use tailwind with hot reloading run `python manage.py tailwind start` in a seperate terminal
 
-## GitHub
+### GitHub
 
 Recommended to use GitHub Desktop <br/>
 
@@ -38,23 +39,23 @@ Recommended to use GitHub Desktop <br/>
 - Send a pull request when branch work is complete
 -
 
-## Make changes to database model
+### Make changes to database model
 
 1. change models in models.py
 2. run `python manage.py makemigrations` to create migrations
 3. run `python manage.py migrate` to apply changes in the db
 
-## Access admin page
+### Access admin page
 
 If you dont have an admin account create one with `python manage.py createsuperuser` <br />
 You dont need an email when you make it, just leave it blank and hit enter <br />
 To access admin page go to `http://127.0.0.1:8000/admin` while the project is running
 
-## After downloading a package
+### After downloading a package
 
 Always run `pip freeze > requirements.txt`
 
-## Naming Conventions
+### Naming Conventions
 
 - Use underscore to seperate words in variables (ex: blue_car)
 - Classes should be uppercase and follow camel case (ex: class Car)
--- src/project/settings.py
+++ src/project/settings.py
@@ -35,41 +35,45 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "app.apps.AppConfig",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+  "app.apps.AppConfig",
+  "django.contrib.admin",
+  "django.contrib.auth",
+  "django.contrib.contenttypes",
+  "django.contrib.sessions",
+  "django.contrib.messages",
+  "django.contrib.staticfiles",
+  "tailwind",
+  "theme",
+  'django_browser_reload'
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+  "django.middleware.security.SecurityMiddleware",
+  "django.contrib.sessions.middleware.SessionMiddleware",
+  "django.middleware.common.CommonMiddleware",
+  "django.middleware.csrf.CsrfViewMiddleware",
+  "django.contrib.auth.middleware.AuthenticationMiddleware",
+  "django.contrib.messages.middleware.MessageMiddleware",
+  "django.middleware.clickjacking.XFrameOptionsMiddleware",
+  "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
+  {
+    "BACKEND": "django.template.backends.django.DjangoTemplates",
+    "DIRS": [],
+    "APP_DIRS": True,
+    "OPTIONS": {
+      "context_processors": [
+        "django.template.context_processors.debug",
+        "django.template.context_processors.request",
+        "django.contrib.auth.context_processors.auth",
+        "django.contrib.messages.context_processors.messages",
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = "project.wsgi.application"
@@ -90,14 +94,14 @@ WSGI_APPLICATION = "project.wsgi.application"
 # }
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.getenv("DB_NAME"),
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST"),
+    "PORT": os.getenv("DB_PORT"),
+  }
 }
 
 
@@ -105,18 +109,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+  {
+    "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+  },
+  {
+    "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+  },
+  {
+    "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+  },
+  {
+    "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+  },
 ]
 
 
@@ -141,3 +145,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#  Django-Tailwind config
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
