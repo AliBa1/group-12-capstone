f9886178b829e3f4d32fb013e0894b4dfa6929e2
Ali <89438079+AliBa1@users.noreply.github.com>
2024-10-30 09:29:28 -0700

    create user, conversation models

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Conversation(models.Model):
+  title = models.CharField(max_length=250, unique=True)
+  # folder = models.
+
+
+class Message(models.Model):
+  timestamp = models.DateTimeField(auto_now_add=True)
+  is_from_user = models.BooleanField(default=False)
+  text = models.TextField()
+  conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+  # map = models.
+  # api_data = models.
