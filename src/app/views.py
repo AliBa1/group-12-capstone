3d096918a20997f0c7ed8337807324e2a70450d6
Ali <89438079+AliBa1@users.noreply.github.com>
2024-10-30 22:44:06 -0700

    chat/explore page layout
    chat/explore page layout, minor header fixes, footer extended

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Conversation, Message
 
 def display_home(request):
   return render(request, "home.html")
@@ -8,4 +8,15 @@ def display_home(request):
 
 @login_required
 def explore_page(request):
+  # if request.method == "POST":
+    # prompt = request.POST.get("message")
+    # conversation title
+    # c_title = request.POST.get("c-title")
+
+    # message = Message.objects.create(is_from_user=True, text=prompt, )
+    
   return render(request, "explore.html")
+
+def conversations_page(request):
+
+  return render(request, "conversations.html")
