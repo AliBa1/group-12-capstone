7f2d61bcf47058b3c0a9bc36603f3669260b113b
Ali <89438079+AliBa1@users.noreply.github.com>
2024-11-24 17:44:55 -0800

    update topics and premade constants

--- src/app/views.py
+++ src/app/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse, HttpResponse, HttpResponseNotFound
 from django.views.decorators.http import require_http_methods
-from app.utils import choose_premade_prompts, is_title_valid
+from app.utils import choose_premade_prompts, choose_topics, is_title_valid
 from .services.travel_service_factory import TravelServiceFactory
 from .services.hotel_service import HotelSearchStrategy
 from .services.flight_service import FlightSearchStrategy
@@ -361,14 +361,13 @@ def update_city_reason(request):
   if request.method == "POST":
     city = request.POST.get("city")
     reason = request.POST.get("reason")
-    temp_conversation = Conversation(city=city, reason=reason)
 
     if city and reason:
       return render(
         request,
         "partials/search.html",
         {
-          "premade_prompts": choose_premade_prompts(temp_conversation),
+          "premade_prompts": choose_topics(reason),
           "city": city,
           "reason": reason,
         },
